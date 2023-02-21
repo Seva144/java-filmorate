be2af3980537fe55a9854ac4d210087c9501229a
Артем Севостьянов <artemsevostanov@MacBook-Air-Artem.local>
2023-02-22 02:30:10 +0300

    TZ11

--- src/test/java/ru/yandex/practicum/javafilmorate/JavaFilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/javafilmorate/JavaFilmorateApplicationTests.java
@@ -1,79 +1,31 @@
 package ru.yandex.practicum.javafilmorate;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.javafilmorate.controllers.FilmController;
-import ru.yandex.practicum.javafilmorate.controllers.UserController;
-import ru.yandex.practicum.javafilmorate.exceptions.NotFoundException;
-import ru.yandex.practicum.javafilmorate.exceptions.ValidationException;
-import ru.yandex.practicum.javafilmorate.model.Film;
 import ru.yandex.practicum.javafilmorate.model.User;
-import ru.yandex.practicum.javafilmorate.service.FilmService;
-import ru.yandex.practicum.javafilmorate.service.UserService;
-import ru.yandex.practicum.javafilmorate.storage.InMemoryFilmStorage;
-import ru.yandex.practicum.javafilmorate.storage.InMemoryUserStorage;
+import ru.yandex.practicum.javafilmorate.storage.FilmDbStorage;
 import ru.yandex.practicum.javafilmorate.storage.UserDbStorage;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-
-import javax.validation.Valid;
-import java.time.LocalDate;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+
 class FilmorateApplicationTests {
-
-	UserController userController;
-	FilmController filmController;
-	FilmService filmService;
-	UserService userService;
-	UserDbStorage userStorage;
-	InMemoryFilmStorage filmStorage;
-
-	User user1;
-	User user2;
-	User user3;
-
-	Film film1;
-	Film film2;
-	Film film3;
-
-	@BeforeEach
-	public void Server_SetUp(){
-		JdbcTemplate jdbcTemplate = new JdbcTemplate();
-
-		userStorage = new UserDbStorage(jdbcTemplate);
-		filmStorage = new InMemoryFilmStorage();
-
-		userService = new UserService(userStorage);
-		filmService = new FilmService(filmStorage, userStorage);
-
-		userController = new UserController(userService);
-		filmController = new FilmController(filmService);
-
-//		user1 = new User("user1@mail.ru", "name1", "login1", LocalDate.of(2000,1,1));
-//		user2 = new User("user2@mail.ru", "name2", "login2", LocalDate.of(2000,1,1));
-//		user3 = new User("user3@mail.ru", "", "login3", LocalDate.of(2000,1,1));
-
-		film1 = new Film("Индиана Джонс", "Известный археолог и специалист ..",
-				LocalDate.of(1981, 1,1), 180);
-		film2 = new Film("Мумия", "Про мумию", LocalDate.of(1999, 1,1), 180);
-		film3 = new Film("Star Wars: Episode I", "Мальчику с далекой планеты суждено изменить судьбу галактики.",
-				LocalDate.of(2000, 1,1), 180);
-	}
+	private final UserDbStorage userStorage;
+	private final FilmDbStorage filmStorage;
 
 	@Test
-	public void Add_Like() throws ValidationException, NotFoundException {
-		userController.createUser(user1);
-		userController.createUser(user2);
-//		filmController.createFilm(film1);
-//		filmController.createFilm(film2);
-//
-//		userController.addFriend(user1.getId(), user2.getId());
+	public void testFindUserById() {
 
+		User user = userStorage.getUser(1);
+
+//		assertThat(user).isPresent()
+//				.hasValueSatisfying(user ->
+//						assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+//				);
 	}
-
-
 }
